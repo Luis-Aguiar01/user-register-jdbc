180bec3592735b16f191f8f0ff4871655ad5ef3d
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-12-12 14:24:55 -0300

    Criando a classe de conexão com o banco

---
+++ src/main/java/br/edu/ifsp/dsw1/model/repository/connection/PostgreSQLConnection.java
@@ -0,0 +1,26 @@
+package br.edu.ifsp.dsw1.model.repository.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLConnection {
+	
+	private static final String URL = "jdbc:postgresql://localhost:5432/exercicio_jdbc";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "Naruto123";
+	
+	private PostgreSQLConnection() {}
+	
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+}
